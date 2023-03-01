47170d553279a778032f7df44256ab5aeb5cbf4e
Inés Victoria Rodríguez <inesvictoria2000@gmail.com>
2023-03-01 09:48:14 +0100

    Primera clase

---
+++ 01-Conceptos-Básicos/01-01-Declaración-de-variables.py
@@ -0,0 +1,28 @@
+#########################################
+# Declaración de variables              #
+#########################################
+#                                       #
+# Ejemplos de declaración de variables  #
+#                                       #
+# Sintaxis: [nombre de la variable]=[]  #
+# Ejemplos:                             #
+#          numero = 20                  #
+#          saludo = "Hola mundo"        #
+#                                       #
+#########################################
+
+# Declaración de variables
+numero = 10
+saludo = "Hola mundo"
+
+print(saludo)
+print(f'El número es {numero}')
+
+# Mostrar el tipo de variables y otras cositas 
+print(type(numero))
+print(type(1==1))
+print(type(('1', '2', '3')))
+print(type(['1', '2', '3']))
+print(type({'1', '2', '3'}))
+print(type([1, 2, 3]))
+      
